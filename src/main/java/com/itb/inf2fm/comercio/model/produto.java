0ed5edfa5f235e52a9bd0802d57bca5a40b4eeef
Leandro Afonso Niches <lecotv2006123@gmail.com>
2023-06-06 12:30:20 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/produto.java
@@ -0,0 +1,17 @@
+package com.itb.inf2fm.comercio.model;
+
+public class produto {
+	
+	private Long id;
+	private String nome;
+	private String descriçao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes.
+	
+	// private: Acesso permtido apenas para os membros da própria classe.
+	//			Entende-se por membros(atributos e métodos)
+	
+
+}
