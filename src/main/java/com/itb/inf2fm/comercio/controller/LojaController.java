46ac4b0fd59f980de5c6a5fb8b1392da885d7b2a
Leandro Afonso Niches <lecotv2006123@gmail.com>
2023-08-07 09:56:28 -0300

    aula 070823

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itb.inf2fm.comercio.model.Produto;
@@ -59,6 +60,15 @@ public class LojaController {
 		model.addAttribute("produto", produto);
 		
 		return "novo-prod";
+	}
+	
+	@PostMapping("/add-prod")
+	public String gravarProduto(Produto produto, Model model) {
+		
+		listaDeProdutos.add(produto);
+		
+		// Redirecionando para uma rota "existente"
+		return "redirect:/comercio/produtos/listar";
 	}
 
 }
